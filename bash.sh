3dfa7f6701e76eff4fe01159173e4debe3148417
401_Unauthorized <iamjosias5@gmail.com>
2025-11-22 21:39:17 +0100

    Project initialized well :)

---
+++ bash.sh
@@ -0,0 +1,7 @@
+echo "# drmn-frontend-api" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/KEMH5/drmn-frontend-api.git
+git push -u origin main
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Interprocessor Communication Workshop</h1>
+    </>
+  )
+}
+
+export default App
